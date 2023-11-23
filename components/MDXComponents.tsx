8a8a52fa53773313bd280f460b3cfb21f30669b9
ines <insanlur@gmail.com>
2023-11-23 18:40:51 +0100

    Remove newsletter

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,5 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
@@ -11,6 +10,5 @@ export const components: MDXComponents = {
   TOCInline,
   a: CustomLink,
   pre: Pre,
-  table: TableWrapper,
-  BlogNewsletterForm,
+  table: TableWrapper
 }
